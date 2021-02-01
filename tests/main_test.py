71d7c22304a816c0b8254883724773ac9cad3370
BobDotCom <BobDotComgt@gmail.com>
2021-01-31 18:48:31 -0600

    idk

---
+++ tests/main_test.py
@@ -0,0 +1,14 @@
+import pytest, time_str, datetime, random
+
+test = (random.randrange(0,61),random.randrange(0,61),random.randrange(0,25),random.randrange(0,366),random.randrange(0,5),random.randrange(0,13),random.randrange(0,100))
+tests = [test for _ in range(1000)]
+
+@pytest.fixture
+def converter():
+    '''Returns a converter'''
+    return time_str.convert
+
+@pytest.mark.parametrize("second,minute,hour,day,week,month,year", tests)
+
+def test_conversions(converter, second, minute, hour, day, week, month, year,):
+    assert converter(f"{second} {random.choice(['seconds', 'second', 'secs', 'sec', 's'])} {minute} {random.choice(['minutes', 'minute', 'mins', 'min', 'm'])} {hour} {random.choice(['hours', 'hour', 'hrs', 'hr', 'h'])} {day} {random.choice(['days', 'day', 'dys', 'dy', 'd'])} {week} {random.choice(['weeks', 'week', 'wks', 'wk', 'w'])} {month} {random.choice(['months', 'month', 'mons', 'mon', 'mn'])} {year} {random.choice(['years', 'year', 'yrs', 'yr', 'y'])}") == datetime.timedelta(seconds=second, minutes=minute, hours=hour, days=day + round(30.5 * month) + (365 * year) + 7 * week)
