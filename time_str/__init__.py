6cb6b9951f402c98d74307efb5be8af4261c65fe
yo56789 <71994752+yo56789@users.noreply.github.com>
2021-08-26 16:25:42 -0400

    fixes

--- time_str/__init__.py
+++ time_str/__init__.py
@@ -56,8 +56,8 @@ class Converter:
             'weeks': ['weeks', 'week', 'wks', 'wk', 'w'], 
             'months': ['months', 'month', 'mons', 'mon', 'mn'], 
             'years': ['years', 'year', 'yrs', 'yr', 'y'],
-            'decades': ['decade', 'decades', 'dcd'],
-            'centurys': ['century', 'centurys', 'c']
+            'decades': ['decade', 'decades', 'dcd', 'dec'],
+            'centuries': ['century', 'centuries', 'c', 'cen']
         }
         self.raw_output = {
             'seconds': 0,
@@ -68,7 +68,7 @@ class Converter:
             'months': 0, 
             'years': 0,
             'decades': 0,
-            'centurys': 0
+            'centuries': 0
         }
 
     def convert(self):
@@ -97,7 +97,7 @@ class Converter:
             self.raw_output['months'] %= 12
         self.raw_output['days'] += round(30.5 * self.raw_output['months']) # datetime.timedelta does not support months
         self.raw_output['days'] += 365 * self.raw_output['years'] # datetime.timedelta does not support years
-        self.raw_output['days'] += 4380 * self.raw_output['decades'] # datetime.timedelta doesnt support decades so we just convert it to days
+        self.raw_output['days'] += 3650 * self.raw_output['decades'] # datetime.timedelta doesnt support decades so we just convert it to days
         self.raw_output['days'] += 36500 * self.raw_output['centurys'] # datetime.timedelta doesnt support centurys
         self.output = datetime.timedelta(seconds=self.raw_output['seconds'], minutes=self.raw_output['minutes'], hours=self.raw_output['hours'], days=self.raw_output['days'], weeks=self.raw_output['weeks'])
         return self.output
